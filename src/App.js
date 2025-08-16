b16fe8f0af2f8586c4e7bba255f32ee64a149fa7
d.litvintsev <d.litvintsev@g.nsu.ru>
2025-08-16 14:08:42 +0800

    adapted header and content for mobile

--- src/App.js
+++ src/App.js
@@ -23,21 +23,20 @@ function App() {
           <a>Материалы</a>
         </div>
 
-        <button onClick={handleOpenModal} className="btn">Связаться</button>
-        <ModalWindow show={showModal} onClose={handleCloseModal}>
-          <h2 style={{color:"#4824ff", fontSize:"40px"}}> Контакты</h2>
-          <p style={{fontSize: "22px"}}>Вы можете написать мне на почту</p>
-        </ModalWindow>
 
-
-        <a href="https://t.me/litvidan" target="_blank"
-        className="icon telegram"/>
-        <a href="https://vk.com/litvidan" target="_blank"
-        className="icon vk"/>
-
-        <div className="switch">
-          <div className="theme light"></div>
+        <div className="header-controls">
+          <button onClick={handleOpenModal} className="btn">Связаться</button>
+          <ModalWindow show={showModal} onClose={handleCloseModal}>
+            <h2 style={{color:"#4824ff", fontSize:"40px"}}> Контакты</h2>
+            <p style={{fontSize: "22px"}}>Вы можете написать мне на почту</p>
+          </ModalWindow>
+          <a href="https://t.me/litvidan" target="_blank"
+          className="icon telegram"/>
+          <a href="https://vk.com/litvidan" target="_blank"
+          className="icon vk"/>
         </div>
+
+
 
       </header>
 
@@ -47,7 +46,7 @@ function App() {
           <div>
           
             <p><h1>Меня зовут Елена Поломошнова</h1>Стать учителем математики я мечтала со школьной скамьи. Окончила Бурятский Государственный университет им. Д.Банзарова. Работаю учителем математики в МБОУ "Нижнесаянтуйская СОШ". Самое прекрасное в моей работе - это дети. Я получаю огромное удовольствие от общения с ними.
-Мой девиз учителя математики: " Учить пониманию, а не запоминанию".</p>
+Мой девиз учителя математики: "Учить пониманию, а не запоминанию".</p>
 
           </div>
 
